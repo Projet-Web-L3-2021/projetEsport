cd62067757225f2e0dfca00555d534acc2b0803d
Alexandre <alexandre.papot.mail@free.fr>
2021-04-13 15:23:02 +0200

    Ajout de l'api rest et affichage dans console.log

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,22 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../services/api.service'
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from './../../services/api.service';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
+
+
+
 export class TestComponent implements OnInit {
+  
+  tournament: string = '';
+  tournaments:any = [];
+  jsonData: any;
 
-  Tournament:any = [];
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
-  constructor(private apiService: ApiService) { }
+  getTournementData() {
+    this.apiService.GetTournament(this.tournament)
+    .subscribe((data: any) => {
+      console.log(data)
+      this.jsonData = data;
 
-  ngOnInit(): void {
-    this.apiService.GetTournament().subscribe(res => {
-      console.log(res)
-      this.Tournament =res;
     });
   }
 
+  ngOnInit(): void {
+  	this.route.paramMap.subscribe(params => {
+      this.tournament = <string>params.get('tournament');
+      this.getTournementData();
+
+    });
+
+    // this.apiService.GetTournament(this.tournament).subscribe(res => {
+    //   console.log(res)
+    //   this.tournaments =res;
+    // });    
+  }
+  
 }
